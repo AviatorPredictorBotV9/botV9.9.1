ad9c36ac6ff1b0af4233a0e0add65e9869d4fef1
AviatorPredictorBotV9 <passionservices25@gmail.com>
2025-04-03 20:15:09 +0300

    Update script.js

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ function submitBettingSiteID() {
 // Activation Code Validation
 function validateActivationCode() {
     const inputCode = document.getElementById("activationCodeInput").value.trim();
-    const correctCode = "GRN250";
+    const correctCode = "GAL753";
 
     if (inputCode === correctCode) {
         document.getElementById("getSignalButton").style.display = "block";
